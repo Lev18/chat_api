d3096e92e5a11f4bc1fa6a9240fe1a0ffcd5c64b
levon <levon>
2025-03-05 19:57:59 +0400

    chat api is working without authentification

--- src/main/java/resource/AuthResource.java
+++ src/main/java/resource/AuthResource.java
@@ -8,6 +8,7 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.validation.Valid;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,21 +18,22 @@ import java.time.Duration;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AuthResource {
-    @ConfigProperty(name = "jwr.issuer")
+    @ConfigProperty(name = "quarkus.smallrye-jwt.issuer")
     String issuer;
 
     @POST
     @Path("/register")
     @Transactional
-    public Response register(AppUser user) {
-        if(user.find("username", user.username)
+    public Response register(@Valid AppUser user) {
+        if(AppUser.find("username", user.username)
                 .firstResultOptional()
                 .isPresent()) {
+            System.out.println(user.username + " user was found ");
             return Response.status(Response.Status.CONFLICT).build();
-            }
+        }
         user.password = PasswordUtil.hashPasswd(user.password);
         user.persist();
-        return Response.ok().build();
+        return Response.ok(user.username + " was registered successfully\n").build();
     }
 
     @POST
@@ -39,19 +41,26 @@ public class AuthResource {
     public Response login(AppUser logReq) {
         AppUser user = AppUser.find("username", logReq.username).firstResult();
         if (user == null) {
+            System.out.println("User is empty");
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
 
         if (!PasswordUtil.checkPasswd(logReq.password, user.password)) {
+
+            System.out.println("User authorization failed ");
+            System.out.println("User password: " + user.password);
+            System.out.println("Login required password: " + logReq.password);
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
 
         Set<String> roleSet = (user.roles != null) ? new HashSet<>(user.roles) : new HashSet<>();
+        /*
         String tok = Jwt.issuer(issuer)
             .upn(user.username)
             .groups(new HashSet<String>(roleSet))
             .expiresIn(Duration.ofHours(1))
             .sign();
-        return Response.ok().entity("{\"token\":\"" + tok + "\"}").build();
+            */
+        return Response.ok().entity("token successfully created!!\n").build();
     }
 }
